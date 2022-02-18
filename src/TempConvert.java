31b6da61f56f9984e91b43f8714bf38d91fae724
AlexLevanchenko <99507725+AlexLevanchenko@users.noreply.github.com>
2022-02-18 10:45:30 +0200

    Update TempConvert.java

--- src/TempConvert.java
+++ src/TempConvert.java
@@ -5,10 +5,10 @@ public class TempConvert {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите температуру в фаренгейте: ");
         final double coefficientT = 1.8;
-        final double zeroCelsius = 32;
+        final double meltingIce = 32;
         float fahrenheit;
         fahrenheit = input.nextInt();
-        double celsius = (fahrenheit-zeroCelsius)/coefficientT;
+        double celsius = (fahrenheit-meltingIce)/coefficientT;
         System.out.println("Температура " +fahrenheit+ " градусов по Фаренгейту равна: "+ celsius +" по Цельсию");
     }
 }
